e2eea78a9506ca78de3391fdf281ef6f10fac9a7
Konstantin <turchakkonstantin@gmail.com>
2023-04-06 16:48:28 +0300

    added layout connection and create pages

---
+++ Easy_DB_Manager/DB.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy_DB_Manager
+{
+    internal class DB
+    {
+        public static Form1 form = new Form1();
+        static string GetConnectionString(string dataSource, string initialCatalog)
+        {
+            return @"Data Source = " + dataSource + "; Initial Catalog = " + initialCatalog + "; Trusted_Connection=True; Encrypt = False";
+        }
+
+        public static async Task CreateDB(string dataSource, string nameDB)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(dataSource, "master")))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand($"CREATE DATABASE {nameDB}", connection);
+                    await command.ExecuteNonQueryAsync();
+                    await connection.CloseAsync();
+
+                    MessageBox.Show($"База данных {nameDB} успешно добавлена", "Выполнено");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message,"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public static async Task CreateTable(string dataSource, string nameDB)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString(dataSource, "master")))
+            {
+                try
+                {
+                    await connection.OpenAsync();
+                    SqlCommand command = new SqlCommand(
+                        $"CREATE DATABASE {nameDB}", connection);
+                    await command.ExecuteNonQueryAsync();
+                    await connection.CloseAsync();
+
+                    MessageBox.Show($"База данных {nameDB} успешно добавлена", "Выполнено");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
--- Easy_DB_Manager/Form1.cs
+++ Easy_DB_Manager/Form1.cs
@@ -1,10 +1,64 @@
+using System.Data.Linq;
+using LinqToDB;
+using LinqToDB.SqlQuery;
+
 namespace Easy_DB_Manager
 {
     public partial class Form1 : Form
     {
+        List<string> columns = new List<string>();
         public Form1()
         {
             InitializeComponent();
+            //MessageBox.Show("Message text",
+            //    "Header", MessageBoxButtons.OK,
+            //    MessageBoxIcon.Error,
+            //    MessageBoxDefaultButton.Button1,
+            //    MessageBoxOptions.DefaultDesktopOnly);
+
+            create_DateBaseNameField.DataBindings.Add(new Binding("Text", connection_DataBaseNameField, "Text"));
+            read_NameBDField.DataBindings.Add(new Binding("Text", connection_DataBaseNameField, "Text"));
+            add_NameBDField.DataBindings.Add(new Binding("Text", connection_DataBaseNameField, "Text"));
+            update_DBNameField.DataBindings.Add(new Binding("Text", connection_DataBaseNameField, "Text"));
+            delete_DBNameField.DataBindings.Add(new Binding("Text", connection_DataBaseNameField, "Text"));
+        }
+
+        //public void ShowMessageBox(string textMesseget, string headerMSB, bool isError)
+        //{
+        //    MessageBox.Show(textMesseget, headerMSB, MessageBoxIcon.)
+        //}
+
+        private void create_NewDB_Click(object sender, EventArgs e)
+        {
+            _ = DB.CreateDB(connection_ServerName.Text, create_DateBaseNameField.Text);
+        }
+
+        private void create_foreignKey_CheckedChanged(object sender, EventArgs e)
+        {
+            create_foreignTableLabel.Visible = create_foreignKey.Checked;
+            create_foreignTableNameField.Visible = create_foreignKey.Checked;
+            create_foreignColumnLabel.Visible = create_foreignKey.Checked;
+            create_foreignColumnNameField.Visible = create_foreignKey.Checked;
+        }
+
+        private void create_default_CheckedChanged(object sender, EventArgs e)
+        {
+            create_defaultValueField.Visible = create_default.Checked;
+        }
+
+        private void create_addColumn_Click(object sender, EventArgs e)
+        {
+            columns.Add($"{create_ColumnsName.Text ?? ""} " +
+                $"{create_dataTypes.SelectedValue?.ToString() ?? ""} ({create_dataTypeRange.Value}) " +
+                $"{create_primaryKey.Text} " +
+                $"");
+
+            create_labelCurrentColumns.Text = "";
+
+            foreach (var item in columns)
+            {
+                create_labelCurrentColumns.Text += item + "\n";
+            }
         }
     }
 }
